ed8833f2a62d66be4302a1f3613b88caf0c2ca1e
azhari.agand <azhari.agand@mandiriinhealth.co.id>
2023-10-09 15:16:16 +0700

    -add endpoint productoffers Signal R

---
+++ Controllers/ProductOfferController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRDemo.Hub;
+
+namespace SignalRDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductOfferController : ControllerBase
+    {
+        private IHubContext<MessageHub, IMessageHubClient> messageHub;
+        public ProductOfferController(IHubContext<MessageHub, IMessageHubClient> _messageHub)
+        {
+            messageHub = _messageHub;
+        }
+        [HttpPost]
+        [Route("productoffers")]
+        public string Get()
+        {
+            List<string> offers = new()
+            {
+                "20% Off on IPhone 12",
+                "15% Off on HP Pavillion",
+                "25% Off on Samsung Smart TV"
+            };
+            messageHub.Clients.All.SendOffersToUser(offers);
+            return "Offers sent successfully to all users!";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using SignalRDemo.Hub;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSignalR();
+builder.Services.AddCors(options => options.AddDefaultPolicy(
+        corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+    ));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapHub<MessageHub>("/offers");
 
 app.UseAuthorization();
 
